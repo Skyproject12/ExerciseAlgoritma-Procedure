f5a7e9208d91634fcf75d0b2de214529dfb5a1bf
sumsky108 <sumendrakadek108@gmail.com>
2019-08-22 14:25:37 +0700

    first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,22 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+    class Kotak {
+        double panjang;
+        double lebar;
+        double tinggi;  //mendefinisikan method void (tidak mengembalikan nilai)
+
+        void cetakVolume() {
+            System.out.println("Volume kotak = " + (panjang * lebar * tinggi));
+        }
+    }
+
+
+    public static void main(String[] args) {
+	// write your code here
+
+
+    }
+}
---
+++ src/com/company/persegi.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class persegi {
+    static int hitungluas(int p, int l){
+        return p*l;
+    }
+    public static void main(String[] args) {
+        int z=0;
+        z=hitungluas(3,2);
+        System.out.println(z);  }
+}
